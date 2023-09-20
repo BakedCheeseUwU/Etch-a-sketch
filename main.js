2e512bab3a3f84317bd32f0a5a9668117b248b69
Aditya <dev.adityasus@gmail.com>
2023-09-20 19:29:29 +0530

    Creates a board of the size decided by the user

--- main.js
+++ main.js
@@ -12,6 +12,27 @@ function createBoard(size) {
   }
 }
 
+// Gets size from the user
+function getSize() {
+  const input = prompt("What should be the size of the box ?");
+  const message = document.querySelector("#message");
+
+  if (!input) {
+    message.innerHTML = "Please provide a number";
+  } else if (input < 0 || input > 100) {
+    message.innerHTML = "Please provide a number between 0 and 100";
+  } else {
+    message.innerHTML = `A board of ${input} x ${input} size is created!!`;
+    return input;
+  }
+}
+
+// Run the following only after HTML and CSS has been loaded
 document.addEventListener("DOMContentLoaded", () => {
   createBoard(16);
+//   creates a board of the inputed size
+  const size_btn = document.querySelector('#popup');
+  size_btn.addEventListener("click",()=>{
+    createBoard(getSize());
+  })
 });
